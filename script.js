9f5ec4cbc37f24315be5694f101c37f48b15ebce
Oke Precious <okepreciousab@gmail.com>
2026-06-09 07:04:19 -1100

    Refactor login and signup forms for improved structure and styling; add createAccount.html for user profile completion

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 
-// Main JavaScript file for Cowrywise project
-// Additional scripts can be added here
-
-
-
-
-
 
 // ===================== SIGN UP ===============================
-const allUserDetails = [];
-console.log(allUserDetails);
+let allUserDetails = JSON.parse(localStorage.getItem('userDatabase')) || [];
+// console.log(allUserDetails);
 
 const enterEmail=()=>{
     for(let index=0; index<allUserDetails.length; index++){
         if(allUserDetails[index].email == emailInput.value){
-            signupErrorMessage.innerHTML = `<p class="text-danger mt-2" style="font-weight: 500;">&#x26A0; An account with this email exists. Please login to your account.</p>`;
+            signupErrorMessage.innerHTML = `<p class="text-danger mt-2" style="font-weight: 500;"><b>&#x26A0;</b> An account with this email exists. Please login to your account.</p>`;
             signupErrorMessage.style.fontSize = "13px";
+            return
         }
-
+        
     }
     if(emailInput.value==""){
         emailInput.style.border = "1px solid red";
         emailLabel.style.color = "red";
-        signupErrorMessage.innerHTML = `<p class="text-danger mt-2" style="font-weight: 500;">&#x26A0; Enter your email address</p>`;
+        signupErrorMessage.innerHTML = `<p class="text-danger mt-2" style="font-weight: 500;"><b>&#x26A0;</b> Enter your email address</p>`;
     }
     else if(!emailInput.value.includes("@") || !emailInput.value.includes(".")){
-        signupErrorMessage.innerHTML = `<p class="text-danger mt-2" style="font-weight: 500;">&#x26A0; Enter a valid email address</p>`;
+        signupErrorMessage.innerHTML = `<p class="text-danger mt-2" style="font-weight: 500;"><b>&#x26A0;</b> Enter a valid email address</p>`;
         signupErrorMessage.style.fontSize = "13px";
         emailInput.style.border = "1px solid red";
         emailLabel.style.color = "red";
@@ -41,6 +35,7 @@ const enterEmail=()=>{
         }
         allUserDetails.push(userDetails)
         localStorage.setItem('userDatabase', JSON.stringify(allUserDetails))
+        wi
         console.log(allUserDetails)
 
     }
